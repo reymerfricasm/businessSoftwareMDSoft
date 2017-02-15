9c99859b48714891de1fceda089fc2c281a1ea43
reymerfricasm <reymerfmoscoso@gmail.com>
2017-02-15 17:18:17 -0400

    Init Again
    Aplication Build on ASP.NET MVC 5. With Login, Modules, Roles &
    DataTable.

---
+++ businessSoftwareMDSoft/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="businessSoftwareMDSoft.MvcApplication" Language="C#" %>
---
+++ businessSoftwareMDSoft/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(businessSoftwareMDSoft.Startup))]
+namespace businessSoftwareMDSoft
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
